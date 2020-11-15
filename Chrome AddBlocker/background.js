f3b0a5bb9ac502d6c7857f6bbdd12c4f1db4cf26
TestForCry <67889458+TestForCry@users.noreply.github.com>
2020-11-15 17:08:36 -0500

    Update background.js

--- Chrome AddBlocker/background.js
+++ Chrome AddBlocker/background.js
@@ -1,4 +1,5 @@
 const defaultFilters = [
+	//Add blocker
 	"*://*.doubleclick.net/*",
 	"*://partner.googleadservices.com/*",
 	"*://*.googlesyndication.com/*",
@@ -9,6 +10,35 @@ const defaultFilters = [
 	"*://*.quantserve.com/*",
 	"*://*.scorecardresearch.com/*",
 	"*://*.zedo.com/*",
+	// This is for Grabify or IP loggers in general 
+	"*://*.grabify.link//*",
+	"*://*.bmwforum.co/*",
+	"*://*.leancoding.co/*",
+	"*://*.spottyfly.com/*",
+	"*://*.stopify.co/*",
+	"*://*.https://yoütu.be/*",
+	"*://*.discörd.com/*",
+	"*://*.minecräft.com/*",
+	"*://*.freegiftcards.co/*",
+	"*://*.disçordapp.com/*",
+	"*://*.xda-developers.us//*",
+	"*://*.quickmessage.us//*",
+	"*://*.fortnight.space/*",
+	"*://*.fortnitechat.site*",
+	"*://*.youshouldclick.us//*",
+	"*://*.joinmy.site/*",
+	"*://*.crabrave.pw/*",
+	"*://*.xn--yotu-1ra.be/*",
+	"*://*.xn--disordapp-s3a.com/*",
+	"*://*.xn--minecrft-5za.com/*",
+	"*://*.xn--discrd-zxa.com/*",
+	"*://*.iplogger.org/*",
+	"*://*.the link/*",
+	"*://*.the link/*",
+	"*://*.the link/*",
+	"*://*.the link/*",
+	"*://*.the link/*",
+	
 ]
 
 chrome.webRequest.onBeforeRequest.addListener(
